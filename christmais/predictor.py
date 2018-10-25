b935b66c8dad6d10f55845e149f8714798ba4ead
ibtingzon <ibtingzon2@gmail.com>
2018-10-25 13:48:53 +0800

    Add predictor

--- christmais/predictor.py
+++ christmais/predictor.py
@@ -12,27 +12,32 @@ import operator
 
 SEED = 42
 
+# init param
 torch.manual_seed(SEED)
 np.random.seed(SEED)
 random.seed(SEED)
 
-LABELS_URL = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
-LABELS = {int(key): value.split(", ") for (key, value) in requests.get(LABELS_URL).json().items()}
-
-IMGNET_MEAN = [0.485, 0.456, 0.406]
-IMGNET_STD = [0.229, 0.224, 0.225]
-
-models = {
-    'resnet152': torchvision.models.resnet152(pretrained=True),
-    'squeezenet1': torchvision.models.squeezenet1_1(pretrained=True),
-    'resnet50': torchvision.models.resnet50(pretrained=True),
-    'vgg16': torchvision.models.vgg16(pretrained=True)
-}
 
 
 class Predictor: 
 
-	def model_eval(self, model, img_variable):    
+	def __init__(self, seed):
+
+		# TODO: If labels is not None, do not download
+		# def _get_labels()
+		self.labels_url = 'https://s3.amazonaws.com/outcome-blog/imagenet/labels.json'
+		self.labels = {int(key): value.split(", ") for (key, value) in requests.get(self.labels_url).json().items()}
+
+		self.models = {
+		    'resnet152': torchvision.models.resnet152(pretrained=True),
+		    'squeezenet1': torchvision.models.squeezenet1_1(pretrained=True),
+		    'resnet50': torchvision.models.resnet50(pretrained=True),
+		    'vgg16': torchvision.models.vgg16(pretrained=True)
+		}
+
+
+
+	def _model_eval(self, model, img_variable):    
 	    """Calculates the probabilities per imagenet class for a given image
 	        
 	    Parameters
@@ -50,12 +55,12 @@ class Predictor:
 	    probs = sm(fc_out) 
 	    return probs.data.numpy()
 
-	def preprocess(self,  img_file):   
+	def _preprocess(self,  img_file):   
 	    """Applies transforms to the input image 
 	        
 	    Parameters
 	    ----------
-	    img_file: str
+	    img_file: PIL.Image
 	        Path to input image 
 
 	    Returns
@@ -64,8 +69,8 @@ class Predictor:
 	        Torch variable of the transformed image
 	    """
 	    normalize = torchvision.transforms.Normalize(
-	       mean=IMGNET_MEAN,
-	       std=IMGNET_STD
+	       mean=[0.485, 0.456, 0.406],
+	       std=[0.229, 0.224, 0.225]
 	    )
 	    preprocess = torchvision.transforms.Compose([
 	       torchvision.transforms.Resize(224),
@@ -74,13 +79,14 @@ class Predictor:
 	       normalize
 	    ])
 	    
-	    img_pil = Image.open(img_file)
+
+	    #img_pil = Image.open(img_file)
 	    img_tensor = preprocess(img_pil)
 	    img_tensor.unsqueeze_(0)
 	    img_variable = Variable(img_tensor)
 	    return img_variable
 
-	def get_score(self, self, inp_file, target_label):
+	def get_score(self, inp_file, target_label):
 	    """Calculates the score for each input image relative to the target label
 	        
 	    Parameters
@@ -95,14 +101,17 @@ class Predictor:
 	    dict
 	        Contains the class probabilities of each imagenet label for each model
 	    """
-	    indices = [index for index, labels in LABELS.items() if target_label == labels[0]]
+
+	    # TODO: Add error catching whenever KeyError/ValueError
+
+	    indices = [index for index, labels in self.labels.items() if target_label == labels[0]]
 	    img_variable = preprocess(inp_file)
 	    
 	    scores, results = [], {}
 	    for model_name, model in models.items():
 	        probs = model_eval(model, img_variable)
 	        scores.append(probs[0][indices[0]])
-	        result = {label[0]: probs[0][index] for index, label in LABELS.items()}
+	        result = {label[0]: probs[0][index] for index, label in self.labels.items()}
 	        results[model_name] = result
 	    
 	    return np.mean(scores), results

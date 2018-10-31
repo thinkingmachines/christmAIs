ffbb712eece2f7379c43f5cbdf10836b1602b327
ljvmiranda921 <ljvmiranda@gmail.com>
2018-10-31 21:21:53 +0800

    [WIP] Add test for predictor

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 
+# Import modules
+import pytest
+
+# Import from package
+from christmais.predictor import Predictor
+
+
+@pytest.mark.parametrize("labels_file", ["labels.json", "extra.json"])
+def test_predictor_get_labels(labels_file):
+    """Test private method _get_labels()"""
+    predictor = Predictor()
+    labels = predictor._get_labels("labels.json")
+    assert isinstance(labels, dict)

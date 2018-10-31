1bc8e705a7d250caa9ebdb113637b8c735ed0d7a
ljvmiranda921 <ljvmiranda@gmail.com>
2018-10-31 22:41:17 +0800

    [WIP] Add test to check predict() output

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -2,9 +2,11 @@
 
 # Import modules
 import pytest
+import numpy as np
 
 # Import from package
 from christmais.predictor import Predictor
+from christmais.drawsys import Artist
 
 
 @pytest.mark.parametrize("labels_file", ["labels.json", "extra.json"])
@@ -28,3 +30,14 @@ def test_predictor_unrecognized_model():
     predictor = Predictor()
     with pytest.raises(KeyError):
         predictor._get_models(["model120"])
+
+
+@pytest.mark.parametrize("model_list", [["resnet152"], ["resnet152", "vgg16"]])
+def test_predict(model_list):
+    """Test if predict returns expected results"""
+    artist = Artist(np.random.uniform(size=8))
+    img = artist.draw()
+    p = Predictor(model_list)
+    score, results = p.predict(img)
+    assert isinstance(score, float)
+    assert isinstance(results, dict)

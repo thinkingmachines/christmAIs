deb43c8c3bfa65ffc191bf9bab6233a640716de3
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-11-09 16:36:03 +0800

    Add methods to expose coords in the image (#21)
    Resolves #21
    
    This commit updates the Artist class in order to get the gene
    of an image and use it for optimization. In addiiton, a draw_from_gene()
    method was implemented in order to draw an image given a certain gene.
    
    Signed-off-by: Lester James V. Miranda <lj@thinkingmachin.es>

--- christmais/drawsys.py
+++ christmais/drawsys.py
@@ -61,13 +61,29 @@ class Artist:
         self.emb = embedding
         self.dims = dims
         self.colors = self._generate_colors(self.emb)
+        # Initialize coordinates
+        self._circle_coords = dict.fromkeys(["layer1", "layer2", "layer3"])
+        self._circle_w = dict.fromkeys(["layer1", "layer2", "layer3"])
+        self._line_coords = []
+        self._line_w = []
 
-    def draw(self, circle_density=10, line_density=10):
-        """Draw the resulting image
+    def draw(self, density=10):
+        """Draw an image based from the embedding
 
         This is the main workhorse for the drawing system. Although the
         background colors were generated randomly, you can override them by
         setting the colors attribute with an RGB tuple
+
+        Note that every time Artist.draw is called, the circle and line
+        coordinates change. Specifically the following attributes are changed:
+            * self._circle_coords
+            * self._circle_w
+            * self._line_coords
+            * self._line_w
+
+        Parameters
+        ----------
+        density
 
         Returns
         -------
@@ -82,12 +98,117 @@ class Artist:
             k: self.colors[k] for k in ("layer1", "layer2", "layer3")
         }
         im, draw = self._draw_circles(
-            im=im, draw=draw, layers=circle_layers, density=circle_density
+            im=im, draw=draw, layers=circle_layers, density=density
         )
         im, draw = self._draw_lines(
-            im=im, draw=draw, color=self.colors["lines"], density=line_density
+            im=im, draw=draw, color=self.colors["lines"], density=density
         )
         return im
+
+    def draw_from_gene(self, gene):
+        """Draw an image based on a gene
+
+        Parameters
+        ----------
+        gene : np.ndarray
+            A matrix of shape (density, 26) for drawing on canvas
+
+        Returns
+        -------
+        PIL.Image
+            The resulting image
+        """
+        # Initialize variables to contain genes
+        # fmt: off
+        circle_coords = dict.fromkeys(["layer1", "layer2", "layer3"])
+        circle_w = dict.fromkeys(["layer1", "layer2", "layer3"])
+        circle_colors = {k: self.colors[k] for k in ("layer1", "layer2", "layer3")}
+        # fmt: on
+        line_coords = gene[:, 21:25]
+        line_w = gene[:, 25]
+        density = gene.shape[0]
+        # Populate circle layers
+        for idx, (layer, _) in enumerate(circle_coords.items()):
+            circle_coords[layer] = gene[:, 6 * idx : 6 * (idx + 1)]
+            circle_w[layer] = gene[:, 18 + idx]
+        # Draw background
+        im = Image.new("RGB", self.dims, self.colors["background"])
+        draw = ImageDraw.Draw(im, "RGBA")
+        # Draw
+        for i, (layer, color) in enumerate(circle_colors.items()):
+            for j in np.arange(density):
+                x1, y1, x2, y2, x3, y3 = circle_coords[layer][j]
+                w = circle_w[layer][j]
+                ## draw ellipses
+                draw.ellipse([x1 - w, y1 - w, x1 + w, y1 + w], fill=color)
+                draw.ellipse([x2 - w, y2 - w, x2 + w, y2 + w], fill=color)
+                draw.ellipse([x3 - w, y3 - w, x3 + w, y3 + w], fill=color)
+        ## draw lines
+        for i in np.arange(density):
+            x1, y1, x2, y2 = line_coords[i]
+            w = line_w[i]
+            width = 2 * w + 2
+            draw.line((x1, y1, x2, y2), fill=self.colors["lines"], width=width)
+            # fmt: off
+            draw.ellipse((x1 - w, y1 - w, x1 + w, y1 + w), fill=self.colors["lines"])
+            draw.ellipse((x2 - w, y2 - w, x2 + w, y2 + w), fill=self.colors["lines"])
+            # fmt: on
+        return im
+
+    def get_gene(self):
+        """Returns the corresponding gene of the last image drawn
+
+        In this case, the gene corresponds to the circle (for each layer) and
+        line positions for a given image in a single matrix. You should expect
+        a matrix with the shape of (density, 26)
+
+        Here's how the value 26 is computed:
+
+            * For the circles, the coordinates have a gene of shape (density,
+            6) and a width (w) of shape (density, 1). Thus, for a single layer,
+            we have a gene with the shape (density, 1 + 6) = (density, 7).
+            Multiply that by three (since we have three layers) and we get a
+            gene of shape (density, 21) for the circles.
+
+            * For the lines, the coordinates have a gene shape of (density, 4)
+            and a width (w) of shape (density, 1). We only have a single line
+            layer, so that makes it (density, 5).
+
+        Then, combining the genes for the circles and lines, we have a shape of
+        (density, 24 + 5) = (density, 26)
+
+        Lastly, the genes are arranged in the following manner (column-wise):
+
+            * self._circle_coords["layer1"] = 6
+            * self._circle_coords["layer2"] = 6
+            * self._circle_coords["layer3"] = 6
+            * self._circle_w["layer1"] = 1
+            * self._circle_w["layer2"] = 1
+            * self._circle_w["layer3"] = 1
+            * self._line_coords = 4
+            * self._line_w = 1
+
+        Returns
+        -------
+        np.ndarray
+            Gene representation of an image
+        """
+        gene = np.hstack(
+            (
+                self._circle_coords["layer1"],
+                self._circle_coords["layer2"],
+                self._circle_coords["layer3"],
+                self._circle_w["layer1"],
+                self._circle_w["layer2"],
+                self._circle_w["layer3"],
+                self._line_coords,
+                self._line_w,
+            )
+        )
+        if gene.all() is None:
+            msg = "Returning None, make sure to call draw() before get_gene()"
+            self.logger.warn(msg)
+        return gene
 
     def _draw_circles(self, im, draw, layers, density=10):
         """Draw circles
@@ -112,11 +233,14 @@ class Artist:
         # Generate candidate coordinates
         cands = self._generate_coords(self.emb, nb_candidates=density)
         # Draw circles
-        for i, (_, color) in enumerate(layers.items()):
+        for i, (layer, color) in enumerate(layers.items()):
             # Make each layer smaller than the one below
             min_width = bottom_min_width / (i + 1)
             max_width = bottom_max_width / (i + 1)
-            for c in cands:
+            # Initialize memory to be filled later
+            w_memory = np.empty([density, 1], dtype=int)
+            coords_memory = np.empty([density, 6], dtype=int)
+            for j, c in enumerate(cands):
                 w = self._interpolate(c[0], target=(min_width, max_width))
                 # Randomly choose a coord from the candidate coords
                 coords_ = np.random.choice(c, size=6)
@@ -124,10 +248,16 @@ class Artist:
                     coords_, target=(w, self.dims[0] - w)
                 )
                 x1, y1, x2, y2, x3, y3 = coords
+                # Put chosen coords and w in memory
+                coords_memory[j] = coords
+                w_memory[j] = w
                 # Draw ellipses
                 draw.ellipse([x1 - w, y1 - w, x1 + w, y1 + w], fill=color)
                 draw.ellipse([x2 - w, y2 - w, x2 + w, y2 + w], fill=color)
                 draw.ellipse([x3 - w, y3 - w, x3 + w, y3 + w], fill=color)
+            # Store coords and w as attributes
+            self._circle_coords[layer] = coords_memory
+            self._circle_w[layer] = w_memory
         return (im, draw)
 
     def _draw_lines(self, im, draw, color, density):
@@ -147,17 +277,26 @@ class Artist:
         min_width = 0.004 * self.dims[0]
         max_width = 0.04 * self.dims[0]
         cands = self._generate_coords(self.emb, nb_candidates=density)
-        for c in cands:
+        # Initialize memory to be filled later
+        w_memory = np.empty([density, 1], dtype=int)
+        coords_memory = np.empty([density, 4], dtype=int)
+        for i, c in enumerate(cands):
             w = self._interpolate(c[0], target=(min_width, max_width))
             width = 2 * w + 2
             # Randomly choose a coord from the candidate coords
             coords_ = np.random.choice(c, size=4)
             coords = self._interpolate(coords_, target=(w, self.dims[0] - w))
             x1, y1, x2, y2 = coords
+            # Put chosen coords and w in memory
+            coords_memory[i] = coords
+            w_memory[i] = w
             # Draw line with round line caps (circles at the end)
             draw.line((x1, y1, x2, y2), fill=color, width=width)
             draw.ellipse((x1 - w, y1 - w, x1 + w, y1 + w), fill=color)
             draw.ellipse((x2 - w, y2 - w, x2 + w, y2 + w), fill=color)
+        # Store coords and w as attributes
+        self._line_coords = coords_memory
+        self._line_w = w_memory
         return (im, draw)
 
     def _generate_coords(self, x, nb_candidates=10):
--- tests/test_drawsys.py
+++ tests/test_drawsys.py
@@ -9,15 +9,15 @@ from PIL import Image, ImageDraw
 from christmais.drawsys import Artist
 
 
-def test_artist_draw(embedding):
+def test_artist_draw_return_type(embedding):
     """Test if Artist.draw() method returns PIL.Image"""
     artist = Artist(embedding)
     img = artist.draw()
     assert isinstance(img, Image.Image)
 
 
-def test_artist_draw_circles(embedding, imdraw_canvas):
-    """Test private method _draw_circles()"""
+def test_artist_draw_circles_return_type(embedding, imdraw_canvas):
+    """Test private method _draw_circles() return type"""
     artist = Artist(embedding)
     im, draw = imdraw_canvas
     layers = {
@@ -29,8 +29,8 @@ def test_artist_draw_circles(embedding, imdraw_canvas):
     assert isinstance(draw_, ImageDraw.ImageDraw)
 
 
-def test_artist_draw_lines(embedding, imdraw_canvas):
-    """Test private method _draw_lines()"""
+def test_artist_draw_lines_return_type(embedding, imdraw_canvas):
+    """Test private method _draw_lines() return type"""
     artist = Artist(embedding)
     im, draw = imdraw_canvas
     color = tuple(np.random.randint(0, 256, size=4))
@@ -39,22 +39,63 @@ def test_artist_draw_lines(embedding, imdraw_canvas):
     assert isinstance(draw_, ImageDraw.ImageDraw)
 
 
-def test_artist_generate_coords(embedding):
-    """Test private method _generate_coords()"""
+def test_artist_generate_coords_return_shape(embedding):
+    """Test private method _generate_coords() shape"""
     artist = Artist(embedding)
     cands = artist._generate_coords(embedding, 10)
     assert cands.shape == (10, 8)
 
 
 @pytest.mark.parametrize("x", [np.random.uniform(size=8), 0.50, [0.25, 0.5]])
-def test_interpolate(embedding, x):
-    """Test private method _interpolate()"""
+def test_interpolate_run_without_fail(embedding, x):
+    """Test private method _interpolate() if it runs without fail"""
     artist = Artist(embedding)
     artist._interpolate(x)
 
 
-def test_generate_colors(embedding):
-    """Test private method _generate_colors"""
+def test_generate_colors_return_type(embedding):
+    """Test private method _generate_colors if it returns the correct type"""
     artist = Artist(embedding)
     colors = artist._generate_colors(embedding)
     assert isinstance(colors, dict)
+
+
+def test_gene_shape(embedding):
+    """Test gene return shape"""
+    artist = Artist(embedding)
+    artist.draw(density=10)
+    gene = artist.get_gene()
+    assert gene.shape == (10, 26)
+
+
+def test_gene_return_values(embedding):
+    """Test if gene return values correspond to the actual layers"""
+    artist = Artist(embedding)
+    artist.draw(density=10)
+    gene = artist.get_gene()
+    assert (artist._circle_coords["layer1"] == gene[:, 0:6]).all()
+    assert (artist._circle_coords["layer2"] == gene[:, 6:12]).all()
+    assert (artist._circle_coords["layer3"] == gene[:, 12:18]).all()
+    assert (artist._circle_w["layer1"].ravel() == gene[:, 18]).all()
+    assert (artist._circle_w["layer2"].ravel() == gene[:, 19]).all()
+    assert (artist._circle_w["layer3"].ravel() == gene[:, 20]).all()
+    assert (artist._line_coords == gene[:, 21:25]).all()
+    assert (artist._line_w.ravel() == gene[:, 25]).all()
+
+
+def test_draw_from_gene_return_type(embedding):
+    """Test if the draw_from_gene() returns a PIL.Image type"""
+    artist = Artist(embedding)
+    artist.draw(density=10)
+    gene = artist.get_gene()
+    img = artist.draw_from_gene(gene)
+    assert isinstance(img, Image.Image)
+
+
+def test_draw_from_gene_return_same_img(embedding):
+    """Test if draw_from_gene() returns the same image"""
+    artist = Artist(embedding)
+    ground_truth_img = artist.draw(density=10)
+    gene = artist.get_gene()
+    test_img = artist.draw_from_gene(gene)
+    assert np.allclose(ground_truth_img, test_img)

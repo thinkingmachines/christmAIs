4ddb16d2e0bc2a6ce5e3cf0dacd93444e3660fe9
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-11-09 16:36:03 +0800

    Add methods to expose coords in the image (#21)
    Resolves #21
    
    This commit updates the Artist class in order to get the gene
    of an image and use it for optimization. In addiiton, a draw_from_gene()
    method was implemented in order to draw an image given a certain gene.
    
    Signed-off-by: Lester James V. Miranda <lj@thinkingmachin.es>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,15 @@ import numpy as np
 import pytest
 from PIL import Image, ImageDraw
 
+# Set random seed
+np.random.seed(42)
+
 
 @pytest.fixture()
 def embedding():
     """Random 8-bit embedding"""
     return np.random.uniform(size=8)
+
 
 @pytest.fixture()
 def imdraw_canvas():

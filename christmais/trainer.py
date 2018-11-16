9aeab5f63bb6041cbc5b1eec85eed799ac984343
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-11-16 09:22:14 +0800

    [WIP] Add _fitness_fcn in trainer

--- christmais/trainer.py
+++ christmais/trainer.py
@@ -21,6 +21,7 @@ class Trainer:
     def __init__(
         self,
         X,
+        population=30,
         dims=(224, 224),
         seed=42,
         predictor_kwargs=None,
@@ -32,6 +33,8 @@ class Trainer:
         ----------
         X : str
             Input sentence to transform into
+        population : int (default is 30)
+            Number of artists created
         dims : tuple of size 2
             Dimensions of the resulting image
         seed : int (default is 42)
@@ -43,25 +46,58 @@ class Trainer:
         """
         self.logger = logging.getLogger(__name__)
         self.X = X
+        self.population = population
         self.dims = dims
         self.seed = seed
         self.predictor_kwargs = predictor_kwargs
         self.embedding_kwargs = embedding_kwargs
         # Create secondary attributes
-        self.artists = None
-        self.model = get_fasttext_pretrained(**embedding_kwargs)
-        self.emb = self.model.transform(X)
+        self.reset()
 
-    def train(self, steps=100, population=30):
+    def reset(self):
+        """Generate a new set of artists, predictor, and model"""
+        self.artists = [
+            Artist(self.emb, self.dims) for i in range(self.population)
+        ]
+        self.logger.info('Created {} artists'.format(len(self.artists)))
+        self.predictor = Predictor(seed=self.seed, **self.predictor_kwargs)
+        self.logger.info(
+            'Initialized Predictor with {}'.format(self.predictor.models)
+        )
+        self.model = get_fasttext_pretrained(**self.embedding_kwargs)
+        self.logger.info('Built FastText model')
+        self.emb = self.model.transform(self.X)
+        self.logger.info('Embedding created for {}'.format(self.X))
+
+    def train(self, target, steps=100, population=30):
         """Train and generate images
 
         Parameters
         ----------
+        target : str
+            The target ImageNet class label.
         steps : int (default is 100)
             The number of steps to run the optimization algorithm
         population : int (default is 30)
             Number of artists created
         """
-        self.artists = [Artist(self.emb, self.dims) for i in range(population)]
-        self.predictor = Predictor(seed=42, **self.predictor_kwargs)
         # imgs = [artist.draw() for artist in self.artists]
+        pass
+
+    def _fitness_fcn(self, imgs, target):
+        """Compute the fitness of the images given a target class label
+
+        Parameter
+        ---------
+        imgs : list of PIL.Image
+            A list of generated images for each Artist
+        target : str
+            The target ImageNet class label.
+
+        Returns
+        -------
+        np.ndarray
+            An array where each element is the fitness of each Artist
+        """
+        fitness = [self.predictor.predict(img, target)[0] for img in imgs]
+        return np.asarray(fitness)

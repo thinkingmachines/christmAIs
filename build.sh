0b58da865468186194dc2990b0ce70d7ac40ffdf
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-11-26 09:29:45 +0800

    Update build script
    Use pip install -e . instead of installing requirements-dev.txt

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
-# Script for a typical Python project
-pip install -r requirements-dev.txt
-python -m nltk.downloader brown
-python -m flake8 christmais
-python -m pytest -v
+# Install dev requirements and package
+pip3 install -r requirements-dev.txt
+pip3 install -e .
+
+# Get brown corpus from nltk
+python3 -m nltk.downloader brown
+
+# Run tests
+python3 -m flake8 christmais
+python3 -m pytest -v

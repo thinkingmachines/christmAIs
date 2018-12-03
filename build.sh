b5c458a96bff6fe20f7923215674a0a1b2a9f435
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-03 13:16:06 +0800

    Remove corpus download in build steps

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 pip3 install -r requirements-dev.txt
 pip3 install -e .
 
-# Get brown corpus from nltk
-python3 -m nltk.downloader brown
-
 # Run tests
 python3 -m flake8 christmais
 python3 -m pytest -v

97ff598881f61071cb64c0e4871a193e1b52f932
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-03 14:12:17 +0800

    Add setup steps for installing magenta

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 pip3 install -r requirements-dev.txt
 pip3 install -e .
 
+# Install magenta (without conda)
+sudo apt-get install -y build-essential libasound2-dev libjack-dev
+pip install magenta
+
 # Run tests
 python3 -m flake8 christmais
 python3 -m pytest -v

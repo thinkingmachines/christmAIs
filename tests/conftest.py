16019463e32bc4fc0ceb1b6725f23cf90c3ceb4c
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-04 12:07:29 +0800

    Add tests for styler

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
 # Import modules
+import glob
 import pytest
 
 # Import from package
 from christmais.parser import Parser
+from christmais.styler import Styler
 
 
 @pytest.fixture
 def parser():
     """Create a pre-made parser"""
     return Parser()
+
+@pytest.fixture
+def styler():
+    """Create a pre-made styler"""
+    return Styler(checkpoint=glob.glob('**/'))
---
+++ tests/test_styler.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for christmais.styler"""
+
+# Import standard library
+import glob
+
+# Import modules
+import numpy as np
+import pytest
+from christmais.styler import Styler

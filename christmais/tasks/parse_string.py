f3fde45d84913ce399bb7b431015dbe30717c616
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-06 10:16:08 +0800

    [ci skip] Add parse_string

---
+++ christmais/tasks/parse_string.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""Utility script for interacting with the Parser class
+
+See Also
+--------
+:class:`christmais.parser.Parser`: the Parser class for string to nearest QuickDraw class
+"""
+
+# Import standard library
+import argparse
+import logging
+
+# Import modules
+from christmais.parser import Parser
+
+# Import from package
+import coloredlogs
+
+
+def build_parser():
+    pass

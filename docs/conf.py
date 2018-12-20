4f67913f5fd7a5c1bc7d56a3914aa52eab6da238
Lester James V. Miranda <ljvmiranda@gmail.com>
2018-12-20 20:41:34 +0800

    Fix conf.py documentation paths

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, '/home/ljvm/Documents/ThinkingMachines/christmAIs')
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+
 import sphinx_bootstrap_theme
+import christmais
 
 # -- Project information -----------------------------------------------------
 
